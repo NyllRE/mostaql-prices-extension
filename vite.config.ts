bd756c82e9ce9cbadfe0808f16d7eb32d5109ecb
NyllRE <nyll.redeast@gmail.com>
2022-11-20 22:44:33 +0300

    initialization

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
+//    "browser_action": {
+//     "default_title": "Vue Mostaql Extension",
+//     "default_popup": "index.html"
+//   }
