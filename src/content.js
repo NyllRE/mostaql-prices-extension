b1a3413b8068daf24230b3367fc2c942352f48ac
NyllRE <nyll.redeast@gmail.com>
2022-11-22 23:35:53 +0300

    replaced old price view with new price view

--- src/content.js
+++ src/content.js
@@ -1,33 +1,42 @@
 const input = document.querySelector("#bid__cost")
 const defaultCostVal = document.querySelector("#bid__realCost")
-const costVal = document.querySelector("#bid-form_container > div:nth-child(1) > div.form-group.col-md-4.col-sm-4.hidden-xs > p")
-
-// costVal.setAttribute("oninput", "convert()");
-
-const convert = () => {
-   costVal.innerHTML = Math.round(Number(input.value) * .80 * 18.6 - 70)
+const price = async () => {
+   return await getCurrency(["TRY"])
 }
+
+//=>> removing the old text displayer
+document.querySelector("#bid-form_container > div:nth-child(1) > div.form-group.col-md-4.col-sm-4.hidden-xs").remove()
+
+//=>> adding a new selector
+const oldCostValue = document
+   .querySelector("#bid-form_container > div:nth-child(1)")
+
+const costValue = document.createElement("div")
+
+oldCostValue.append(costValue)
+
+
+costValue.innerHTML = `<h3 style="display: grid; place-items: center">Add price to view</h3>`
+
+
+
 input.addEventListener("input", async () => {
-   convert()
+   await convert(costValue)
 })
 
-
-defaultCostVal.addEventListener("input", () => {
-   console.log('e3')
-})
-defaultCostVal.innerHTML.addEventListener("change", () => {
-   console.log('e3')
-})
-
-defaultCostVal.innerHTML.onChange = function () {
-   alert('eeee3')
-   console.log('eee3')
+const convert = async (displayText) => {
+   const Lira = await price()
+   const numToTL = Math.round(Number(input.value) * Lira * .80)
+   displayText.style.direction = `ltr`
+   displayText.innerHTML =
+      `<h4 style="display: grid; place-items: center;margin: 1.7em 0">${numToTL} - 70 = ${numToTL - 70}TL</h4>`
 }
-defaultCostVal.addEventListener("propertychange", () => {
-   console.log('e3')
-})
 
+const getCurrency = async (currencies) => {
 
-function sleep(ms) {
-   return new Promise(resolve => setTimeout(resolve, ms));
+   return await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=sSLJqP6lGUy4wgKna5CEVjSgcThLX1ml0eY9ZN8G&currencies=${currencies.join("%2C") || 'EUR%2CUSD%2CCAD%2CTRY'}`)
+      .then((response) => response.json())
+      .then((data) => {
+         return data.data.TRY
+      });
 }
