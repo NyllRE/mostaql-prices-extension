4f90ed5b330775943d4c6ed2c0122c6e1191530b
riobits <23579928+riobits@users.noreply.github.com>
2022-11-23 22:37:37 +0300

    Change files to typescript

--- src/content.ts
+++ src/content.ts
@@ -1,72 +1,52 @@
-/** @format */
+const input = document.querySelector<HTMLInputElement>('#bid__cost')
 
-import { HtmlInputsOptions } from 'vite-plugin-chrome-extension/types/plugin-options';
-
-const input = document.querySelector<HTMLInputElement>('#bid__cost');
-// const defaultCostVal: HTMLInputElement | null =
-// document.querySelector('#bid__realCost');
-
-let price: Promise<number>;
+let price: Promise<number>
 //=>> getting the price
 const priceGetter = async (): Promise<number> => {
-	return await getCurrency(['TRY']);
-};
+  return await getCurrency(['TRY'])
+}
 
 //=>> removing the old text displayer
 document
-	.querySelector(
-		'#bid-form_container > div:nth-child(1) > div.form-group.col-md-4.col-sm-4.hidden-xs'
-	)
-	?.remove();
+  .querySelector(
+    '#bid-form_container > div:nth-child(1) > div.form-group.col-md-4.col-sm-4.hidden-xs'
+  )
+  ?.remove()
 
 //=>> adding a new selector
 const oldCostValue = document.querySelector(
-	'#bid-form_container > div:nth-child(1)'
-);
-const costValue = document.createElement('div');
+  '#bid-form_container > div:nth-child(1)'
+)
+const costValue = document.createElement('div')
 
 window.addEventListener('load', async () => {
-	price = priceGetter();
-	costValue.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">
+  price = priceGetter()
+  costValue.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">
    Add price to view in html
-   </h4>`;
-	oldCostValue?.append(costValue);
-	console.log('hi', costValue.innerHTML);
-});
+   </h4>`
+  oldCostValue?.append(costValue)
+})
 
 input?.addEventListener('input', async () => {
-	await convert(costValue);
-});
+  await convert(costValue)
+})
 
 const convert = async (displayText) => {
-	const numToTL = Math.round(Number(input?.value) * (await price) * 0.8);
-	displayText.style.direction = `ltr`;
-	displayText.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">${numToTL} - 70 = ${
-		numToTL - 70
-	}TL</h4>`;
-};
+  const numToTL = Math.round(Number(input?.value) * (await price) * 0.8)
+  displayText.style.direction = `ltr`
+  displayText.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">${numToTL} - 70 = ${
+    numToTL - 70
+  }TL</h4>`
+}
 
 const getCurrency = async (currencies): Promise<number> => {
-	return await fetch(
-		`https://api.freecurrencyapi.com/v1/latest?apikey=sSLJqP6lGUy4wgKna5CEVjSgcThLX1ml0eY9ZN8G&currencies=${
-			currencies.join('%2C') || 'EUR%2CUSD%2CCAD%2CTRY'
-		}`
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			return data.data.TRY;
-		});
-};
-
-const loadHTML = async (file) => {
-	return await fetch(file)
-		.then((response) => response.text())
-		.then((text) => {
-			console.log(text);
-			return text;
-		});
-};
-
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+  return await fetch(
+    `https://api.freecurrencyapi.com/v1/latest?apikey=sSLJqP6lGUy4wgKna5CEVjSgcThLX1ml0eY9ZN8G&currencies=${
+      currencies.join('%2C') || 'EUR%2CUSD%2CCAD%2CTRY'
+    }`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data.data.TRY
+    })
 }
