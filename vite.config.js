22e1a7fdc131c746dcad78ad90484388a9fa47f0
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-11-23 18:07:11 +0300

    added typescript

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import { crx } from "@crxjs/vite-plugin";
+// import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [crx({ manifest })],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
+	build: {
+		rollupOptions: {
+			input: "manifest.json"
+		}
+	},
+	plugins: [
+		chromeExtension()
+	],
 });
 //    "browser_action": {
 //     "default_title": "Vue Mostaql Extension",
