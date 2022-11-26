4c7fcbdaff170cb6eadb8e3aec44d8c99e878b0b
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-11-26 22:31:04 +0300

    add price limit to in-site displayer

--- src/api.ts
+++ src/api.ts
@@ -5,17 +5,21 @@ interface ConvertOptions {
 }
 
 export const convert = async ({
-  displayText,
-  originalPrice,
-  currencies,
+	displayText,
+	originalPrice, //=> in USD
+	currencies,
 }: ConvertOptions) => {
-  const price = await getCurrency([...currencies])
-  const numToTL = Math.round(originalPrice * price * 0.8)
-  displayText.style.direction = `ltr`
-  displayText.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">${numToTL} - 70 = ${
-    numToTL - 70
-  }TL</h4>`
-}
+	if (originalPrice < 25) {
+		displayText.innerHTML = `your fee should be 25$ or more`;
+		return;
+	}
+	const price = await getCurrency([...currencies]);
+	const numToTL = Math.round(originalPrice * price * 0.8 * 0.96);
+	displayText.style.direction = `ltr`;
+	displayText.innerHTML = `<h4 style="display: grid; place-items: center;margin: 1.7em 0">${numToTL} - 70 = ${
+		numToTL - 70
+	}₺</h4>`;
+};
 
 export const getCurrency = async (currencies: string[]): Promise<number> => {
   return await fetch(
